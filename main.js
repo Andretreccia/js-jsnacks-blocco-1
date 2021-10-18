43fa30a8dcae0bfa6d1ea8b478e5d4dd3281dc36
Andretreccia <andreabtreccia@gmail.com>
2021-10-18 15:28:39 +0200

    Completamento snack2
    utilizzando la stessa logica del precedente

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//snack 1
+///////////////////////////////////  SNACK 1
 const snack1 = document.querySelector(".snack1")
 
 let result1 = "";
@@ -24,4 +24,32 @@ function snack1btn() {
     }
     //stampa il risultato su schermo
     snack1.innerHTML = result1
+}
+
+/////////////////////////////////    SNACK 2
+
+const snack2 = document.querySelector(".snack2")
+
+let result2 = "";
+//al click del bottone 1
+function snack2btn() {
+    //chiedi all utente di inserire due numeri in successione
+    let fistWord = prompt("inserisci una parola");
+    let secondWord = prompt("inseriscri un'altra parola");
+
+
+    //verifica quale delle parole sia la più lunga
+    if (fistWord.length > secondWord.length) {
+        result2 = `<p>La parola più lunga è: ${fistWord}. Con una lunghzza di ${fistWord.length} caratteri!</p>`;
+
+    }
+    else if (fistWord.length < secondWord.length) {
+        result2 = `<p><p>La parola più lunga è: ${secondWord}. Con una lunghzza di ${secondWord.length} caratteri!</p>`;
+    }
+    else {
+        result2 = `<p>Le due parole hanno la stessa lunghezza. ${secondWord.length} caratteri!</p>`;
+
+    }
+    //stampa il risultato su schermo
+    snack2.innerHTML = result2
 }
